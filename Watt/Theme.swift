b8f760ddc060e64a1e3ee879c37e08eea38e5843
David Albert <davidbalbert@gmail.com>
2023-10-04 16:10:57 +0300

    Load theme from .xccolortheme file

--- Watt/Theme.swift
+++ Watt/Theme.swift
@@ -10,20 +10,7 @@ import Cocoa
 struct Theme {
     let attributes: [Token.TokenType: AttributedRope.Attributes]
 
-    // Hack until https://github.com/apple/swift/issues/60574 is fixed
-    typealias A = AttributedRope.Attributes
-
-    static let defaultTheme: Theme = [
-        .keyword: A.foregroundColor(.systemBlue).symbolicTraits(.italic),
-        .string: A.foregroundColor(.systemGreen),
-        .type: A.foregroundColor(.systemOrange).symbolicTraits(.bold),
-        .function: A.foregroundColor(.systemPurple),
-        .constant: A.foregroundColor(.systemTeal),
-        .variable: A.foregroundColor(.systemPink),
-        .delimiter: A.foregroundColor(.systemGray),
-        .number: A.foregroundColor(.systemBrown),
-        .operator: A.symbolicTraits(.italic).underlineColor(.black).underlineStyle(.thick),
-    ]
+    static let defaultTheme: Theme = try! Theme(contentsOfXCColorThemeURL: URL(filePath: "/Applications/Xcode.app/Contents/SharedFrameworks/DVTUserInterfaceKit.framework/Versions/A/Resources/FontAndColorThemes/Default (Light).xccolortheme"))
 
     subscript(key: Token.TokenType) -> AttributedRope.Attributes? {
         var type: Token.TokenType? = key
@@ -45,8 +32,81 @@ struct Theme {
     }
 }
 
-extension Theme: ExpressibleByDictionaryLiteral {
-    init(dictionaryLiteral elements: (Token.TokenType, AttributedRope.Attributes)...) {
-        self.attributes = Dictionary(uniqueKeysWithValues: elements)
+struct XCColorTheme: Decodable {
+    struct Color: Decodable {
+        let red: CGFloat
+        let green: CGFloat
+        let blue: CGFloat
+        let alpha: CGFloat
+
+        init(from decoder: Decoder) throws {
+            let container = try decoder.singleValueContainer()
+            let string = try container.decode(String.self)
+            let components = string.split(separator: " ").compactMap { Double($0) }
+            guard components.count == 4 else {
+                throw DecodingError.dataCorruptedError(in: container, debugDescription: "Invalid color string")
+            }
+            self.red = CGFloat(components[0])
+            self.green = CGFloat(components[1])
+            self.blue = CGFloat(components[2])
+            self.alpha = CGFloat(components[3])
+        }
+    }
+
+    // load the colors from the DVTSourceTextSyntaxColors key
+    let colors: [String: Color]
+
+    enum CodingKeys: String, CodingKey {
+        case colors = "DVTSourceTextSyntaxColors"
+    }
+}
+
+extension Token.TokenType {
+    init?(xcColorThemeKey: String) {
+        switch xcColorThemeKey {
+            case "xcode.syntax.keyword":
+                self = .keyword
+            case "xcode.syntax.string":
+                self = .string
+            case "xcode.syntax.declaration.type":
+                self = .type
+            case "xcode.syntax.identifier.function":
+                self = .function
+            case "xcode.syntax.identifier.function.system":
+                self = .functionSpecial
+            case "xcode.syntax.identifier.constant":
+                self = .constant
+            case "xcode.syntax.identifier.variable":
+                self = .variable
+            case "xcode.syntax.number":
+                self = .number
+            default:
+                return nil
+        }
+    }
+}
+
+extension NSColor {
+    convenience init(xcColorSchemeColor color: XCColorTheme.Color) {
+        self.init(red: color.red, green: color.green, blue: color.blue, alpha: color.alpha)
+    }
+}
+
+extension Theme {
+    // initialize a theme from an Xcode theme file URL
+    init(contentsOfXCColorThemeURL url: URL) throws {
+        let data = try Data(contentsOf: url)
+        let decoder = PropertyListDecoder()
+        let xcColorTheme = try decoder.decode(XCColorTheme.self, from: data)
+
+        var attributes: [Token.TokenType: AttributedRope.Attributes] = [:]
+        for (key, color) in xcColorTheme.colors {
+            guard let tokenType = Token.TokenType(xcColorThemeKey: key) else {
+                continue
+            }
+            attributes[tokenType] = AttributedRope.Attributes.foregroundColor(NSColor(xcColorSchemeColor: color))
+        }
+
+        self.attributes = attributes
     }
 }
