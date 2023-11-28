98af7359b50cbf0721bc6a03e7ebebbd97b7cad5
David Albert <davidbalbert@gmail.com>
2023-11-28 12:12:17 -0500

    Utilities: simpler isEqual

--- Watt/Utilities.swift
+++ Watt/Utilities.swift
@@ -7,31 +7,6 @@
 
 import Foundation
 
-fileprivate protocol EquatablePair {
-    func perform() -> Bool
-}
-
-fileprivate protocol MaybeEquatablePair {
-    func maybePerform() -> Bool?
-}
-
-fileprivate struct Pair<T> {
-    var a: T
-    var b: T
-}
-
-extension Pair: MaybeEquatablePair {
-    func maybePerform() -> Bool? {
-        (self as? EquatablePair)?.perform()
-    }
-}
-
-extension Pair: EquatablePair where T: Equatable {
-    func perform() -> Bool {
-        a == b
-    }
-}
-
 func isEqual(_ a: Any?, _ b: Any?) -> Bool {
     switch (a, b) {
     case (.none, .none):
@@ -41,15 +16,18 @@ func isEqual(_ a: Any?, _ b: Any?) -> Bool {
     case (.some(_), .none):
         return false
     case let (.some(a), .some(b)):
-        func helper<T>(_ a: T) -> Bool {
-            if let b = b as? T {
-                return Pair(a: a, b: b).maybePerform() ?? false
+        func helper<E>(_ a: E) -> Bool where E: Equatable {
+            if let b = b as? E {
+                return a == b
             } else {
                 return false
             }
         }
 
-        // Swift 6: this can be changed to `return helper(a)`
-        return _openExistential(a, do: helper)
+        guard let a = a as? any Equatable else {
+            return false
+        }
+
+        return helper(a)
     }
 }
