2ef422d321c1ef38682e79528ca4a8832fbb9ecb
David Albert <davidbalbert@gmail.com>
2023-11-30 17:17:17 -0500

    Remove unused Collection extensions

--- StandardKeyBindingResponder/Tests/StandardKeyBindingResponderTests/Support/Foundation+Extensions.swift
+++ StandardKeyBindingResponder/Tests/StandardKeyBindingResponderTests/Support/Foundation+Extensions.swift
@@ -13,22 +13,6 @@ extension StringProtocol {
     }
 }
 
-extension Collection {
-    func index(after i: Index, clampedTo upperBound: Index) -> Index {
-        index(i, offsetBy: 1, limitedBy: upperBound) ?? upperBound
-    }
-
-    func index(_ i: Index, offsetBy distance: Int, clampedTo limit: Index) -> Index {
-        index(i, offsetBy: distance, limitedBy: limit) ?? limit
-    }
-}
-
-extension BidirectionalCollection {
-    func index(before i: Index, clampedTo lowerBound: Index) -> Index {
-        index(i, offsetBy: -1, limitedBy: lowerBound) ?? lowerBound
-    }
-}
-
 extension Range where Bound == Int {
     init(_ range: Range<String.Index> , in string: String) {
         let start = string.utf8.distance(from: string.utf8.startIndex, to: range.lowerBound)
