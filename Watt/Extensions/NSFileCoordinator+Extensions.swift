085c47645bc7259a5d7e77f667670a342e9e24b9
David Albert <davidbalbert@gmail.com>
2024-01-30 17:23:44 -0500

    Support cancellation in async NSFileCoordinator extensions

--- Watt/Extensions/NSFileCoordinator+Extensions.swift
+++ Watt/Extensions/NSFileCoordinator+Extensions.swift
@@ -7,24 +7,40 @@
 
 import Foundation
 
+fileprivate struct Cancel: @unchecked Sendable {
+    let coordinator: NSFileCoordinator
+
+    func callAsFunction() {
+        coordinator.cancel()
+    }
+}
+
 extension NSFileCoordinator {
     // TODO: replace with isolated(caller) or similar when this proposal gets implemented: https://forums.swift.org/t/pitch-inheriting-the-callers-actor-isolation/68391
 
-    // While I haven't observed any deadlocks like I did with NSFilePromiseReceiver.receivePromisedFiles(atDestination:options:operationQueue:), I think this is still unsafe to use without inheriting our parent's actor isolation – NSFileCoordinator is unsafe
+    // While I haven't observed any deadlocks like I did with NSFilePromiseReceiver.receivePromisedFiles(atDestination:options:operationQueue:), I think this is still unsafe to use without inheriting our parent's actor isolation – NSFileCoordinator is not sendable
     @_unsafeInheritExecutor
     func coordinate<T>(with intents: [NSFileAccessIntent], queue: OperationQueue, byAccessor accessor: @escaping @Sendable () throws -> T) async throws -> T {
-        try await withCheckedThrowingContinuation { continuation in
-            coordinate(with: intents, queue: queue) { error in
-                if let error {
-                    continuation.resume(throwing: error)
-                } else {
-                    do {
-                        continuation.resume(returning: try accessor())
-                    } catch {
+        // NSFileCoordinator.cancel() is thread-safe, so we wrap it in an unchecked Sendable struct.
+        let cancel = Cancel(coordinator: self)
+
+
+        return try await withTaskCancellationHandler {
+            try await withCheckedThrowingContinuation { continuation in
+                coordinate(with: intents, queue: queue) { error in
+                    if let error {
                         continuation.resume(throwing: error)
+                    } else {
+                        do {
+                            continuation.resume(returning: try accessor())
+                        } catch {
+                            continuation.resume(throwing: error)
+                        }
                     }
                 }
             }
+        } onCancel: {
+            cancel()
         }
     }
 
