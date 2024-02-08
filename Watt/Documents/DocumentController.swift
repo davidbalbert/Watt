4d8e149ee7537a71c9f85dd9a7e85abb5d186e83
David Albert <davidbalbert@gmail.com>
2024-02-08 10:52:37 -0700

    Don’t include workspace files in Open Recent

--- Watt/Documents/DocumentController.swift
+++ Watt/Documents/DocumentController.swift
@@ -14,15 +14,35 @@ class DocumentController: NSDocumentController {
         static let substituteDocumentURL = "substituteDocumentURL"
     }
     static var substituteDocumentURL: URL?
+    var skipNoteNextRecentDocument: Bool = false
 
     override func beginOpenPanel(_ openPanel: NSOpenPanel, forTypes inTypes: [String]?, completionHandler: @escaping (Int) -> Void) {
         openPanel.canChooseDirectories = true
         super.beginOpenPanel(openPanel, forTypes: inTypes, completionHandler: completionHandler)
     }
 
+    override func noteNewRecentDocument(_ document: NSDocument) {
+        if skipNoteNextRecentDocument {
+            skipNoteNextRecentDocument = false
+            return
+        }
+        super.noteNewRecentDocument(document)
+    }
+
     override static func restoreWindow(withIdentifier identifier: NSUserInterfaceItemIdentifier, state: NSCoder, completionHandler: @escaping (NSWindow?, Error?) -> Void) {
         substituteDocumentURL = state.decodeObject(of: NSURL.self, forKey: RestorationKeys.substituteDocumentURL) as? URL
         super.restoreWindow(withIdentifier: identifier, state: state, completionHandler: completionHandler)
+    }
+
+    override func openDocument(withContentsOf url: URL, display displayDocument: Bool, completionHandler: @escaping (NSDocument?, Bool, Error?) -> Void) {
+        super.openDocument(withContentsOf: url, display: displayDocument) { document, displayDocument, error in
+            if error != nil {
+                // If the open fails, noteNewRecentDocument doesn't get called, so reset it here
+                self.skipNoteNextRecentDocument = false
+            }
+
+            completionHandler(document, displayDocument, error)
+        }
     }
 
     override func reopenDocument(for urlOrNil: URL?, withContentsOf contentsURL: URL, display displayDocument: Bool, completionHandler: @escaping (NSDocument?, Bool, Error?) -> Void) {
