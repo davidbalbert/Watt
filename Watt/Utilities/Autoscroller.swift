6ed5a43ccbc696bb4da025336bb6c5ac6d4bd8e2
David Albert <davidbalbert@gmail.com>
2024-03-19 12:35:15 -0400

    Autoscroll the same regardless of refresh rate

--- Watt/Utilities/Autoscroller.swift
+++ Watt/Utilities/Autoscroller.swift
@@ -20,7 +20,7 @@ fileprivate let mouseEvents: [NSEvent.EventType] = [
     .mouseMoved
 ]
 
-// A helper class for smooth autoscrolling.
+// A helper for smooth autoscrolling.
 class Autoscroller {
     // TODO: for whatever reason, I'm not actually able to get 120 Hz working on my M1 MacBook Pro. Investigate.
     static var preferredFrameRateRange: CAFrameRateRange {
@@ -37,6 +37,9 @@ class Autoscroller {
 
     private var locationInWindow: NSPoint
 
+    // For small enough dx and dy, the scroll view doesn't end up scrolling. When that happens, accumulate
+    // the dx and dy each frame so that we can still scroll when the pointer is very close to the edge of
+    // the view.
     private var dxRemainder: CGFloat
     private var dyRemainder: CGFloat
 
@@ -97,21 +100,29 @@ class Autoscroller {
         }
 
         let viewportBounds = scrollView.contentView.bounds
+        let dt = sender.targetTimestamp - sender.timestamp
+        let hz = (1/dt).rounded()
 
-        let slowdown: CGFloat = 16
-        let maxScroll: CGFloat = 256
+        // minScroll is set up so that we end up scrolling at least one point per second, or 0.5 points per
+        // 0.5 seconds, which seems to be the granularity of an NSScrollView on a 2x display.
+        //
+        // maxScroll=256 and slowdown=(1/16) feel right for 60 Hz. To keep the same feel for other refresh
+        // rates, we scale these values. Slowdown ends up squared, so we have to scale it by the square root.
+        let minScroll: CGFloat = dt
+        let maxScroll: CGFloat = 256.0 * (60.0/hz)
+        let slowdown: CGFloat = (1.0/16.0) * sqrt(60/hz)
 
         let dx: CGFloat
         if view.bounds.width <= viewportBounds.width {
             dx = 0
         } else if locationInView.x <= viewportBounds.minX {
-            let d = (viewportBounds.minX - locationInView.x) / slowdown
-            let scaled = min(maxScroll, d*d)
-            dx = -scaled
+            let d = (viewportBounds.minX - locationInView.x) * slowdown
+            let scaled = (d*d).clamped(to: minScroll...maxScroll)
+            dx = -scaled + min(dxRemainder, 0)
         } else if locationInView.x >= viewportBounds.maxX {
-            let d = (locationInView.x - viewportBounds.maxX) / slowdown
-            let scaled = min(maxScroll, d*d)
-            dx = scaled
+            let d = (locationInView.x - viewportBounds.maxX) * slowdown
+            let scaled = (d*d).clamped(to: minScroll...maxScroll)
+            dx = scaled + max(dxRemainder, 0)
         } else {
             dx = 0
         }
@@ -120,13 +131,13 @@ class Autoscroller {
         if view.bounds.height <= viewportBounds.height {
             dy = 0
         } else if locationInView.y <= viewportBounds.minY {
-            let d = (viewportBounds.minY - locationInView.y) / slowdown
-            let scaled = min(maxScroll, d*d)
-            dy = -scaled
+            let d = (viewportBounds.minY - locationInView.y) * slowdown
+            let scaled = (d*d).clamped(to: minScroll...maxScroll)
+            dy = -scaled + min(dyRemainder, 0)
         } else if locationInView.y >= viewportBounds.maxY {
-            let d = (locationInView.y - viewportBounds.maxY) / slowdown
-            let scaled = min(maxScroll, d*d)
-            dy = scaled
+            let d = (locationInView.y - viewportBounds.maxY) * slowdown
+            let scaled = (d*d).clamped(to: minScroll...maxScroll)
+            dy = scaled + max(dyRemainder, 0)
         } else {
             dy = 0
         }
@@ -141,6 +152,15 @@ class Autoscroller {
         let y = (scrollOffset.y + dy).clamped(to: 0...(view.bounds.height - viewportBounds.height))
 
         view.scroll(NSPoint(x: x, y: y))
+
+        // NSClipView rounds its bounds origin and only scrolls when abs(newX-oldX) >= a threshold. Experimentally
+        // on a 2x display, that threshold is 0.5. But rather than hardcoding, just check to see if we've actually
+        // scrolled, and if we haven't save dy and dx to accumulate on the next frame.
+        let newScrollOffset = scrollView.contentView.bounds.origin
+        let xScrolled = abs(newScrollOffset.x - scrollOffset.x) > 1e-10
+        let yScrolled = abs(newScrollOffset.y - scrollOffset.y) > 1e-10
+        dxRemainder = xScrolled ? 0 : dx
+        dyRemainder = yScrolled ? 0 : dy
 
         callback(NSPoint(x: locationInView.x + dx, y: locationInView.y + dy))
     }
